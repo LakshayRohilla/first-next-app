04ef45bf8b8d546b1c112966ca1928f564042704
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2023-12-22 23:30:57 +0530

    links between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,4 +1,17 @@
 // These pages contains normal react js component
+import Link from 'next/link';
 export default function NewsPage(){
-    return <h1> This is a news page !!</h1>
+    return (
+        <>
+          <h1>The News Page</h1>
+          <ul>
+            <li>
+            <Link href='/news/nextjs-is-a-great-framework'>
+                NextJS Is A Great Framework
+              </Link>
+            </li>
+            <li>Something Else</li>
+          </ul>
+        </>
+      );
 }
